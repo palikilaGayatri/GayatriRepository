8f16e9329aa095296781e8446ca3bc2240a64620
palikilaGayatri <102041802+palikilaGayatri@users.noreply.github.com>
2022-04-03 15:35:08 +0530

    Add files via upload

---
+++ Functions.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_H_INCLUDED
+#define FUNCTIONS_H_INCLUDED
+
+int dayNumber(int day, int month, int year);
+char* getMonthName(int monthNumber);
+int numberOfDays(int monthNumber, int year);
+void printCalendar(int year);
+
+#endif
---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+#include “Functions.h”   
+
+int main()
+{
+    int year = 2016;
+  
+    // Function Call
+    printCalendar(year);
+    return 0;
+}
+ 
+
